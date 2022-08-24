f9bc4949ff0022085fd8a01dbfba729f461a6efb
Matiarriete <47977071+Matiarriete@users.noreply.github.com>
2022-08-24 21:31:49 +0200

    Cambios nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,28 +1,29 @@
 var nav = document.querySelector('nav');
-        var activado = 0;
+        var scrolleado = 0;
+        var resize = 0;
         if (screen.width < 768) {
             nav.classList.add('bg-dark', 'shadow', 'text-center');
-            activado = 1;
+            resize = 1;
         }
         window.addEventListener('scroll', function () {
-            if (activado == 0) {
+            if (resize == 0) {
                 if (window.pageYOffset > 100) {
                     nav.classList.add('bg-dark', 'shadow', 'text-center');
-                    activado = 1;
+                    scrolleado = 1;
                 } else {
                     nav.classList.remove('bg-dark', 'shadow', 'text-center');
-                    activado = 0;
+                    scrolleado = 0;
                 }
             }
         });
         window.addEventListener('resize', function () {
-            if (activado == 0) {
+            if (scrolleado == 0) {
                 if (screen.width < 768) {
                     nav.classList.add('bg-dark', 'shadow', 'text-center');
-                    activado = 1;
+                    resize = 1;
                 } else {
                     nav.classList.remove('bg-dark', 'shadow', 'text-center');
-                    activado = 0;
+                    resize = 0;
                 }
             }
         });
